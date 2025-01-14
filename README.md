6da477a23d78c9275a7d6eab3c18cf913122405a
zhizuozhang <zhizuozhang@gmail.com>
2025-01-14 14:19:36 +0800

    update 1880 interval dp

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Luogu
 + **P1616**: [easy][dp][knapsack]
 + **P1776**: [medium][dp][knapsack]
 + **P1833**: [medium][dp][knapsack]
++ **P1880**: [easy][dp][interval_dp]
 + **P2024**: [medium][union_set]
 +
--- problems/Luogu_P1880.cpp
+++ problems/Luogu_P1880.cpp
@@ -49,11 +49,74 @@ int main() {
     std::cin >> N;
     std::vector<int> vec(2*N+1, 0);
     std::vector<int> pre(2*N+2, 0); // pre[i] - pre[i-1] = vec[i-1]
-    std::vector<int> dp
+    std::vector<std::vector<int> > dp(2*N+1, std::vector<int>(2*N+1, 0));
     for (int i = 0; i < N; ++i) {
         std::cin >> vec[i];
         vec[N+i] = vec[i];
     }
+    for (int i = 1; i <=  2 * N + 1; ++i) {
+        pre[i] = pre[i-1] + vec[i-1];
+    } // vec[i] = pre[i+1] - pre[i];
+    // vec[i, ..., j] = pre[j+1]] - pre[i];
+
+    // dp[i][j] 表示区间 i, j的极值是多少
+    // 转移方程 dp[i][j] = max(dp[i][j], dp[i]][k] + dp[k+1][j] + sum[j+1] - sum[i])
+    // min
+    for (int i = 0; i < 2*N+1; ++i) {
+        for (int j = i; j < 2*N+1; ++j) {
+            dp[i][j] = 9999999999;
+        }
+    }
+    for (int i = 0; i < 2*N+1; ++i) {
+        dp[i][i] = 0;
+    }
+    for (int i = 0; i < 2 * N + 1 - 1; ++i) {
+        dp[i][i+1] = vec[i] + vec[i+1];
+    }
+    for (int len = 2; len <= N; len++) {
+        for (int i = 1; i + len <= 2 * N; i++) {
+            int j = i + len - 1;
+            for (int k = i; k < j; k++) {
+                dp[i][j] = std::min(dp[i][j], dp[i][k] + dp[k+1][j] + pre[j+1] - pre[i]);
+            }
+        }
+    }
+    int res_min = 9999999999;
+    for (int i = 0; i < N; ++i) {
+        res_min = std::min(res_min, dp[i][i+N-1]);
+    }
+    std::cout << res_min << std::endl;
+
+    // max
+    for (int i = 0; i < 2*N+1; ++i) {
+        for (int j = i; j < 2*N+1; ++j) {
+            dp[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < 2 * N + 1 - 1; ++i) {
+        dp[i][i+1] = vec[i] + vec[i+1];
+    }
+    for (int len = 2; len <= N; len++) {
+        for (int i = 1; i + len <= 2 * N; i++) {
+            int j = i + len - 1;
+            for (int k = i; k < j; k++) {
+                dp[i][j] = std::max(dp[i][j], dp[i][k] + dp[k+1][j] + pre[j+1] - pre[i]);
+            }
+        }
+    }
+    int res_max = 0;
+    for (int i = 0; i < N; ++i) {
+        res_max = std::max(res_max, dp[i][i+N-1]);
+    }
+    std::cout << res_max << std::endl;
 
     return 0;
 }
+
+/*
+
+g++ ./problems/Luogu_P1880.cpp -o ./efiles/test
+
+./efiles/test
+
+ */
