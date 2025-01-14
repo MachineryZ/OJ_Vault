c3ff7b5a17e04473a16526cf1ae6691267f9eb7a
zhizuozhang <zhizuozhang@gmail.com>
2025-01-14 16:58:48 +0800

    update 5549

--- problems/Luogu_P5569.cpp
+++ problems/Luogu_P5569.cpp
@@ -46,3 +46,49 @@ $ N \leq 40000, a_i \leq 200$
 #include <iostream>
 #include <vector>
 
+int main() {
+    int N;
+    std::cin >> N;
+    std::vector<int> vec(N, 0);
+    std::vector<int> pre(N+1, 0); // pre[i] - pre[i-1] = vec[i-1]
+    std::vector<std::vector<int> > dp(N, std::vector<int>(N, 0));
+    for (int i = 0; i < N; ++i) {
+        std::cin >> vec[i];
+    }
+    for (int i = 1; i <= N; ++i) {
+        pre[i] = pre[i-1] + vec[i-1];
+    }
+    for (int i = 0; i < N; ++i) {
+        for (int j = i; j < N; ++j) {
+            dp[i][j] = 99999999;
+        }
+    }
+    for (int i = 0; i < N; ++i) {
+        dp[i][i] = 0;
+    }
+    for (int i = 0; i < N-1; ++i) {
+        dp[i][i+1] = vec[i] + vec[i+1];
+    }
+    for (int len = 2; len <= N; len++) {
+        for (int i = 0; i + len <= N; i++) {
+            int j = i + len - 1;
+            for (int k = i; k < j; k++) {
+                // std::cout << i << " " << k << " " << j << std::endl;
+                dp[i][j] = std::min(dp[i][j], dp[i][k] + dp[k+1][j] + pre[j+1] - pre[i]);
+            }
+        }
+    }
+    std::cout << dp[0][N-1] << std::endl;
+    return 0;
+
+}
+
+/*
+
+g++ ./problems/Luogu_P5569.cpp -o ./efiles/test
+
+./efiles/test
+
+
+
+*/
